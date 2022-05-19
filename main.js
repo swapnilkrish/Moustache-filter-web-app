cdecbdb695621158b0ebacee9a6c39915b067aff
swapnilkrish <86659449+swapnilkrish@users.noreply.github.com>
2022-05-19 15:21:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 function preload() {
+    moustache = loadImage("Moustache.png");
 }
 
 function setup() {
     canvas = createCanvas(300, 300);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('poseNet is intialized');
 }
 
 function draw() {
